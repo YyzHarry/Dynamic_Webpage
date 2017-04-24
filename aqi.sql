c57112d3869fa3d3aa844cba128921a0a96fe7ba
YyzHarry <1400012996@pku.edu.cn>
2017-04-24 21:05:26 +0800

    Add a SQL table, for demo.

---
+++ aqi.sql
@@ -0,0 +1,113 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: aqidata
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aqi_average`
+--
+
+DROP TABLE IF EXISTS `aqi_average`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aqi_average` (
+  `district_id` int(11) NOT NULL,
+  `district_name` varchar(45) NOT NULL,
+  `CO` int(11) NOT NULL,
+  `NO` int(11) NOT NULL,
+  `SO2` int(11) NOT NULL,
+  `O3` int(11) NOT NULL,
+  `PM1` int(11) NOT NULL,
+  `PM2` int(11) NOT NULL,
+  `PM10` int(11) NOT NULL,
+  PRIMARY KEY (`district_id`,`district_name`,`CO`,`NO`,`SO2`,`O3`,`PM1`,`PM2`,`PM10`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aqi_average`
+--
+
+LOCK TABLES `aqi_average` WRITE;
+/*!40000 ALTER TABLE `aqi_average` DISABLE KEYS */;
+INSERT INTO `aqi_average` VALUES (0,'百讲',12,12,32,312,2,31,1),(1,'二教',4,3,34,4,32,5,23),(2,'南门',3,4,1,2,3,4,3),(3,'西门',5,45,34,567,3,12,21),(4,'图书馆',6,6,4,76,6,6,5),(5,'邱德拔',2,3,4,6,4,6,0),(6,'未名湖',5,4,3,2,1,9,0);
+/*!40000 ALTER TABLE `aqi_average` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aqi_now`
+--
+
+DROP TABLE IF EXISTS `aqi_now`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aqi_now` (
+  `district_id` int(11) NOT NULL,
+  `district_name` varchar(45) NOT NULL,
+  `CO` int(11) NOT NULL,
+  `NO` int(11) NOT NULL,
+  `SO2` int(11) NOT NULL,
+  `O3` int(11) NOT NULL,
+  `PM1` int(11) NOT NULL,
+  `PM2` int(11) NOT NULL,
+  `PM10` int(11) NOT NULL,
+  PRIMARY KEY (`district_id`,`district_name`,`CO`,`NO`,`SO2`,`O3`,`PM1`,`PM2`,`PM10`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aqi_now`
+--
+
+LOCK TABLES `aqi_now` WRITE;
+/*!40000 ALTER TABLE `aqi_now` DISABLE KEYS */;
+INSERT INTO `aqi_now` VALUES (0,'百讲',0,0,0,0,0,0,0),(1,'二教',1,1,1,1,1,1,1),(2,'南门',2,2,2,2,2,2,2),(3,'西门',3,3,3,3,3,3,3),(4,'图书馆',4,4,4,4,4,4,4),(5,'邱德拔',5,5,5,5,5,5,5),(6,'未名湖',6,6,6,6,6,6,6);
+/*!40000 ALTER TABLE `aqi_now` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `repository`
+--
+
+DROP TABLE IF EXISTS `repository`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `repository` (
+  `district_id` int(11) NOT NULL,
+  `district_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`district_id`,`district_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `repository`
+--
+
+LOCK TABLES `repository` WRITE;
+/*!40000 ALTER TABLE `repository` DISABLE KEYS */;
+INSERT INTO `repository` VALUES (0,'百讲'),(1,'二教'),(2,'南门'),(3,'西门'),(4,'图书馆'),(5,'邱德拔'),(6,'未名湖');
+/*!40000 ALTER TABLE `repository` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-08 21:55:30
